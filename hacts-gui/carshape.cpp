f10a32e02f9d64262fa52040c51cc154cb54ae4b
Karol Baraniecki <karol@karcom.pl>
2018-05-23 17:22:16 +0200

    arrow

--- hacts-gui/carshape.cpp
+++ hacts-gui/carshape.cpp
@@ -8,7 +8,12 @@
 CarShape::CarShape(QGraphicsItem *parent) :
     QGraphicsPolygonItem(parent)
 {
-    const QVector<QPointF> shape { {0, -2}, {-2, -3.3}, {0, 3.3}, {2, -3.3} };
+    const qreal width = 3.23, length = 4.02;
+    const qreal halfwidth = width/2, halflength = length/2;
+
+    const QVector<QPointF> shape {
+        {halfwidth, -halflength}, {halfwidth, halflength}, {-halfwidth, halflength}, {-halfwidth, -halflength}
+    };
     setPolygon(shape);
 
     setBrush(QBrush(QColor("#6495ED")));
@@ -16,4 +21,12 @@ CarShape::CarShape(QGraphicsItem *parent) :
     QPen pen;
     pen.setWidthF(0.1);
     setPen(pen);
+
+    QGraphicsPolygonItem *arrow = new QGraphicsPolygonItem(this);
+    arrow->setBrush(QBrush(QColor("#FF6666")));
+    const QVector<QPointF> arrowShape {
+        {0, -.6}, {-.6, -1.0}, {0, 1.0}, {.6, -1.0}
+    };
+    arrow->setPolygon(arrowShape);
+    arrow->setPen(pen);
 }
